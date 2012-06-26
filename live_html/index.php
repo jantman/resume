6f35b73d1d2486d2897e1436f4eecbd823a1ed7f
Jason Antman <jason@jasonantman.com>
2012-06-26 13:21:39 +0000

    what im looking for added back again

--- live_html/index.php
+++ live_html/index.php
@@ -47,7 +47,17 @@ $mtimeStr = date("Y-m-d", $mtime);
 </p>
 
 <p><span class="areatitle">What I&#39;m looking for:</span></p>
-<p class="indented"> </p>
+<p class="indented">I am an enthusiastic, talented, self-motivated system
+  administrator/engineer looking for a mid- to senior-level operations/DevOps
+  position in an agile environment. I'm obsessed with monitoring, performance
+  analysis, automation, and developing tools to make operations more
+  efficient and reliable. I'm looking for an environment where sysadmins are
+  inventors, tinkerers, developers, scientists, and problem solvers on the
+  macro scale. I want to code myself out of a job, and believe that machine
+  provisioning should be a point-and-click task, allowing my team to focus on
+  bigger problems. If things like large-scale log analysis, dashboard
+  creation, automated provisioning, and idempotent configuration management
+  are in your every day vocabulary, I'd probably enjoy working with you.</p>
 
 <p><span class="areatitle">A bit about me:</span></p>
 
